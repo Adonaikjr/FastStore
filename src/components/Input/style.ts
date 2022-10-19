6294326a07adc682284c7e9e0887f8c2c606454f
adonaikjr <adonaikjr@gmail.com>
2022-10-19 19:01:35 -0300

    fast store

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,14 +1,11 @@
-import  styled  from 'styled-components';
+import styled from 'styled-components'
 export const ContainerInput = styled.div`
-width: 60%;
-height: 3rem;
-margin:  8.31rem 0rem 4.18rem 0rem;
-input{
+  width: 60%;
+  height: 3rem;
+  margin: 8.31rem 0rem 4.18rem 0rem;
+  input {
     width: 100%;
     height: 100%;
     border-radius: 8px;
-    background-color: ${(props) => props.theme['base_input']};
-    border: solid 1px ${(props) => props.theme['base_border']};
-    
-}
+  }
 `
